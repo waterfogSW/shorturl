885b26dfc189a34833acf5a97dee982f6e1d4a62
waterfog9580 <waterfog9580@gmail.com>
2023-01-19 11:14:11 +0900

    :heavy_plus_sign: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+  testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+  testImplementation("io.kotest:kotest-assertions-core:5.5.4")
 }
 
 tasks.withType<KotlinCompile> {
